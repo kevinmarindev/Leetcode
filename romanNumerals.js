2388a4f7e7c826992c70cd080226c186b2d9b6a9
kevinmarindev <kevin.marin7@hotmail.com>
2022-05-16 22:52:13 -0500

    roman numeral solution

---
+++ romanNumerals.js
@@ -0,0 +1,33 @@
+var romanToInt = function(s){
+    let total = 0
+    const valuesInIt = {
+        "I": 1,
+       "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    };
+    // let totalsInS = Object.values(valuesInIt)
+    // console.log(totalsInS)
+
+    let array2 = s.split('').map(item => valuesInIt[item]);
+
+    // console.log(array2)
+
+    // array2.forEach((item, idx, array) =>{
+    //     item < array[idx + 1] ? total++ : ''
+    // });
+
+    for (let i = 0; i <= array2.length + 1; i++) {
+        if(array2[i] >= array2[i + 1]) total += array2[i] 
+        if(array2[i] < array2[i + 1]) array2[i + 1] = array2[i + 1] - array2[i]
+        if(i == array2.length - 1) total += array2[array2.length - 1]
+        console.log(i, array2, total)
+    }
+
+    return total
+}
+
+console.log(romanToInt("III"))
