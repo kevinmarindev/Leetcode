b0ddff062d568ecbad87658c757280ee99f6a989
kevinmarindev <kevin.marin7@hotmail.com>
2022-05-24 00:17:10 -0500

    solved the algo

---
+++ longestCommonPrefix.js
@@ -0,0 +1,15 @@
+var longestCommonPrefix = function(strs) {
+    const keyStr = strs[0];
+    let returnStr = "";
+    if(strs[0] == '') return ''
+    for (let i = 0; i < keyStr.length; i++) {
+        if(strs.length == strs.filter(word => word[i] == keyStr[i]).length) returnStr += keyStr[i]
+       else break
+    }
+    return returnStr
+};
+
+console.log(longestCommonPrefix(['flower', 'flower', 'flower']))
+
+
+// console.log('thecatsr'.includes('the'))
