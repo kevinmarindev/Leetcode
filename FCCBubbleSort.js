d7c35f2e436918fb143c86f5dea17fdeddcb2688
kevinmarindev <kevin.marin7@hotmail.com>
2022-08-11 20:59:13 -0500

    solved and optimized bibble sort

--- FCCBubbleSort.js
+++ FCCBubbleSort.js
@@ -63,3 +63,48 @@ console.log(bubbleSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]))
 // [1,2,4,8,123,345,43]
 //current = 345 i = 5
     //set array[i] = array[i + 1], array[i + 1] = current 
+
+
+//PROBLEM
+//ACTUAL IMPLEMENTATION OF BUBBLE SORT
+
+//BREAK DOWN
+//given an unsorted arr return a sorted arry by comparing 2 continious values and decreasing the amount of comparisons by one on each iteration 
+
+//I1: unsorted arr   OUTPUT: sorted array
+
+// function bubbleSort2(arr){
+    
+//     for (let i = arr.length - 1; i >= 0 ; i--) {
+//         for (let j = 0; j < arr.length; j++) {
+//             console.log(arr, arr[j], arr[j + 1])
+//             let temp = arr[j]
+//             if(arr[j] > arr[j + 1]) arr[j] = arr[j + 1], arr[j + 1] = temp 
+//         }
+//         console.log('completed on whole iteration')
+//     }
+//     return arr
+// }
+
+// console.log(bubbleSort2([1,3,7,8,3,5,46,9]))
+
+// this solution does the same amount of comparisons on every loop so it is not optimized to reduce the amount of comaprisons by one after every inner loop iteration.
+
+
+//Even more optimized solution 
+// reduces the amount of comparasons by one on every loop since we know that every iteration places the biggest num at the end. 
+function bubbleSort3(arr){
+    
+    for (let i = arr.length - 1; i > 0 ; i--) {
+        for (let j = 0; j < i - 1; j++) {
+            //by checking that j is less than i we make sure not to go up all the way to the nums that have already been sorted
+            console.log(arr, arr[j], arr[j + 1])
+            let temp = arr[j]
+            if(arr[j] > arr[j + 1]) arr[j] = arr[j + 1], arr[j + 1] = temp 
+        }
+        console.log('completed on whole iteration')
+    }
+    return arr
+}
+
+console.log(bubbleSort3([1,3,7,8,3,5,46,9]))
