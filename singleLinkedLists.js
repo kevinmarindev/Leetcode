53d1a5588c9d0e2091ba2a388ba89d3d0f488257
kevinmarindev <kevin.marin7@hotmail.com>
2022-09-11 01:46:46 -0500

    working on adding the push method to the list

--- singleLinkedLists.js
+++ singleLinkedLists.js
@@ -4,6 +4,20 @@ class SingleLinkList {
         this.tail = null;
         this.length = 0;
     }
+    //creating the push method which adds an item to the end of the list
+    push(value){
+        if(this.length < 1){
+            this.head = value;
+            this.tail = value;
+        }else{
+            this.tail = value
+        }
+        this.length++
+    }
 }
 
 const listOne = new SingleLinkList();
+
+
+//adding items to this thing
+listOne.push('kevin')
