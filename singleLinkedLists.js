6ea80df514a497a39d79fd23faab22ea4eae329a
kevinmarindev <kevin.marin7@hotmail.com>
2022-09-13 18:50:37 -0500

    added shift method to list

--- singleLinkedLists.js
+++ singleLinkedLists.js
@@ -33,27 +33,39 @@ class SingleLinkList {
     pop(){
         //cant pop items if there are none
         if(this.length < 1) return undefined
+
         let currentNode = this.head
         let removedItem  
         while(currentNode){
-        if(currentNode.next === this.tail){
-             removedItem = this.tail
-             this.tail = currentNode 
-             currentNode.next = null 
-            
-            // console.log(current.next, this.tail)
-        }
-        currentNode = currentNode.next
-        // this.length = this.length - 1
+            if(currentNode.next === this.tail){
+                removedItem = this.tail
+                this.tail = currentNode 
+                currentNode.next = null 
+            }
+            currentNode = currentNode.next
         }
         this.length--
+        if(this.length === 0) this.head = null, this.tail = null
         return removedItem
+    }
+    //add shift methods whcih removes the 1st item from the list. 
+    shift(){
+        if(this.length < 1) return undefined
+        const oldHead = this.head;
+        this.head = this.head.next;
+        this.length--
+        if(this.length === 0) this.tail = null
+        return oldHead
     }
 }
 
 const listOne = new SingleLinkList();
 
 
-//adding items to this thing
-// listOne.push('kevin')
+
+listOne.push('kevin')
+listOne.push('jacob')
+// listOne.push('marin')
+console.log(listOne)
+
 
