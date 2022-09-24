03bc1ad4e6886f1745ab27b916697f0a7550e8ad
kevinmarindev <kevin.marin7@hotmail.com>
2022-09-23 23:58:49 -0500

    implement push method

--- doubleLinkedLists.js
+++ doubleLinkedLists.js
@@ -11,6 +11,34 @@ class DoubleLinkedList{
         this.head = null
         this.tail = null
         this.length = 0 
-
+    }
+    push(val){
+        const newNode = new Node(val)
+        if(this.length > 0){
+            console.log('rur')
+            const OldTail = this.tail 
+            OldTail.next = newNode
+            this.tail = newNode
+            this.tail.previous = OldTail
+        }else{
+            console.log('oo')
+            this.head = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
     }
 }
+
+
+let list = new DoubleLinkedList()
+console.log(list)
+
+list.push('kevin')
+console.log(list)
+
+list.push('mar')
+console.log(list)
+
+list.push('lon')
+console.log(list)
