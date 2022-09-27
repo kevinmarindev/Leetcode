eceaa03b8d04ca74c8176aa8c20b91c45893ae3f
kevinmarindev <kevin.marin7@hotmail.com>
2022-09-26 23:21:58 -0500

    Working on double lined lists

---
+++ doubleLinkedLists.js
@@ -0,0 +1,16 @@
+class Node{
+    constructor(value){
+        this.value = value 
+        this.next = null
+        this.previous = null 
+    }
+}
+
+class DoubleLinkedList{
+    constructor(){
+        this.head = null
+        this.tail = null
+        this.length = 0 
+
+    }
+}
