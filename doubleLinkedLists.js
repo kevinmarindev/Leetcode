5fc320d23cbe40423edca27f2d80ac4e56854c5c
kevinmarindev <kevin.marin7@hotmail.com>
2022-10-08 01:04:42 -0500

    unshift method added

--- doubleLinkedLists.js
+++ doubleLinkedLists.js
@@ -58,6 +58,19 @@ class DoubleLinkedList{
         this.length--
         return odldHead
     }
+    unshift(val){
+        const newNode = new Node(val)
+        if(this.length < 1){
+            this.head =  newNode
+            this.tail = newNode
+        }else{
+            this.head.previous = newNode
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
     
 }
 
