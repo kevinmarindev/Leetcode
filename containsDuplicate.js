dbb1fb512e954d5dbbbc7e7731a06602bde41b56
kevinmarindev <kevin.marin7@hotmail.com>
2025-07-10 19:16:29 -0500

    solving more leetcodes

---
+++ containsDuplicate.js
@@ -0,0 +1,39 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+
+// Output: true
+
+// Explanation:
+
+// The element 1 occurs at the indices 0 and 3.
+
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+
+// Output: false
+
+// Explanation:
+
+// All elements are distinct.
+
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+// Output: true
+
+//! 07/10/25
+var containsDuplicate = function (nums) {
+  if (nums.length == 1) return false;
+  const collector = {};
+
+  for (const num of nums) {
+    collector[num] ? collector[num]++ : (collector[num] = 1);
+    if (collector[num] > 1) return true;
+  }
+  return false;
+};
